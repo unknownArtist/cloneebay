17bb210f355f7e671ababab7fdb050be820ec2dd
gufranciit <gufranciit@gmail.com>
2012-09-20 17:13:27 +0500

    some changes

--- application/controllers/SearchController.php
+++ application/controllers/SearchController.php
@@ -10,7 +10,26 @@ class SearchController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        // action body
+    	$cat = $_POST['Category'];
+    	$text = $_POST['searchtxt'];
+
+    	if(empty($text))
+    	{
+	    	$where = "category = '$cat'";
+	    	$products = new Application_Model_Products();
+	        $data = $products->fetchAll($where)->toArray();
+	        $this->view->items = $data;
+    	}
+
+    	else
+    	{
+
+	    	$where = "category = '$cat' AND description LIKE '$text'";
+	    	$products = new Application_Model_Products();
+	        $data = $products->fetchAll($where)->toArray();
+	        $this->view->items = $data;
+    	}
+
     }
 
 
